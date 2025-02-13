52cd34f3bcbbf92a13e8a64fc110c9e0f8f4d53d
adamyrzakanova <aidaimyrzakanova@usf.edu>
2025-02-13 14:52:44 -0500

    Task 6 - Inventory Stock Management.

--- cc_example.js
+++ cc_example.js
@@ -13,3 +13,34 @@ function findOrders(orders, orderId) {
 some_order = findOrders(orders, 101);
 console.log(`${some_order.customer} has an order id ${some_order.id} with a total of ${some_order.total}`);
 
+
+
+// Task 6
+
+let inventory = {
+    Items :[],
+    addItem(name, quantity){
+        //This method adds an item to items array
+        this.Items.push({name, quantity});  
+    },
+    removeLatestItem() {
+        //this method removes the last item
+        this.Items.pop();
+    },
+    removeFirstItem() {
+        //this method removes the first item
+        this.Items.shift();
+    }
+    };
+    inventory.addItem("Monitor", 5);
+    inventory.addItem("Keyboard", 10);
+    inventory.addItem("Mouse", 3);
+
+    console.log(inventory.Items);//Viewing all items 
+    inventory.removeLatestItem();//Removing the last items
+    console.log(inventory.Items);//Viewing items remaining
+    inventory.removeFirstItem();``//Removing the first item
+    console.log(inventory.Items);//Viewing items remaining.
+    //inventory.removeFirstItem(); //Removing the first item
+    //console.log(inventory.Items);//Viewing items remaining.
+    
